1c1383673d585f9d44f78c6b05fceb8e4d56a857
JIuMoHkA <JIuMoHkA99@mail.ru>
2022-07-01 01:43:59 +0400

    HelloWorld Added

---
+++ SkillMod2/Program.cs
@@ -0,0 +1,25 @@
+﻿string fullName = "Иванов Иван Иванович";
+byte age = 30;
+string email = "blablabla@mail.ru";
+double pointsProg = 40.0;
+double pointsMath = 30.43;
+double pointsPhys = 55.345;
+
+Console.WriteLine("Задание 1");
+Console.WriteLine("Имя----------------------: " + fullName
+                 + "\nВозраст------------------: " + age
+                 + "\nEmail--------------------: " + email
+                 + "\nБаллы по программированию: " + pointsProg
+                 + "\nБаллы по математике------: " + pointsMath
+                 + "\nБаллы по физике----------: " + pointsPhys);
+Console.ReadKey();
+Console.WriteLine("\nЗадание 2");
+double sum = pointsProg + pointsMath + pointsPhys;
+double arMean = sum / 3;
+Console.WriteLine("Сумма: {0} + {1} + {2} = {3}",
+                    pointsProg,
+                    pointsMath,
+                    pointsPhys,
+                    sum);
+Console.WriteLine("Среднее арифметическое: " + arMean);
+Console.ReadKey(); 
--- Skillb1/Program.cs
+++ Skillb1/Program.cs
@@ -4,8 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!  <= (Задание 1)");
+            Console.Write("Hello ");
+            Console.Write("World ");
+            Console.Write("!!!");
+            Console.Write(" <= (Задание 2)");
+
             Console.ReadKey();
         }
     }
 }
+
+//В новейшей версии .NET объявление namespace, using и тела main - опускается и дополняется программой. 
+//Что сокращает выполнение задания по условия модуля 1 до одной строки. 
